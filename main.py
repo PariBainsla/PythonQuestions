9fafc1bb04c300c4f54301ba23a6dcc89f1fdb31
PariBainsla <paribainsla161@gmail.com>
2025-09-30 16:24:30 -0700

    another question

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # Accept numbers from a user
-no = int(input("enter some number -- "))
-print(no)
+# no = int(input("enter some number -- "))
+# print(no)
 
+# Accept age from the user
+age = int(input("What is your age? ---"))
+print(age)
